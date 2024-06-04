66dc92c345445ff23a8a3da52bd6d39b485d1996
edvaldoljr <edvaldoluizj@gmail.com>
2024-06-04 11:25:19 -0300

    Alteração no codigo

--- src/javaBasico/Ex4.java
+++ src/javaBasico/Ex4.java
@@ -9,18 +9,32 @@ public class Ex4 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        float n1, n2, n3,n4, diferecaProduto, calculo;
-        System.out.println("Digite o n1: ");
-        n1 = entrada.nextFloat();
-        System.out.println("Digite o n2: ");
-        n2 = entrada.nextFloat();
-        System.out.println("Digite o n3: ");
-        n3 = entrada.nextFloat();
-        System.out.println("Digite o n4: ");
-        n4 = entrada.nextFloat();
-        calculo = (n1 * n2 ) - (n3 * n4);
-        diferecaProduto = calculo;
-        System.out.println("A diferença do produto: " + diferecaProduto);
+        float[] valores = new float[4];
+        float calculo, diferencaProduto;
+
+        for (int i = 0; i < valores.length; i++ ) {
+            System.out.printf("Digite a numero %d: ", i + 1);
+            valores[i] = entrada.nextFloat();
+        }
+
+        calculo = (valores[0] * valores[1]) - (valores[2] * valores[3]);
+        diferencaProduto = calculo;
+        System.out.println();
+
+        System.out.println("A diferença do produto: " + diferencaProduto);
+
+//        float n1, n2, n3,n4, diferencaProduto, calculo;
+//        System.out.println("Digite o n1: ");
+//        n1 = entrada.nextFloat();
+//        System.out.println("Digite o n2: ");
+//        n2 = entrada.nextFloat();
+//        System.out.println("Digite o n3: ");
+//        n3 = entrada.nextFloat();
+//        System.out.println("Digite o n4: ");
+//        n4 = entrada.nextFloat();
+//        calculo = (n1 * n2 ) - (n3 * n4);
+//        diferecaProduto = calculo;
+//        System.out.println("A diferença do produto: " + diferencaProduto);
     }
 
 }
