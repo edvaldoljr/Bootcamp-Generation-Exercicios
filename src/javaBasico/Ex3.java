42c1dc676c38e6d96f02c36c4fa771db0692380a
edvaldoljr <edvaldoluizj@gmail.com>
2024-06-04 13:22:17 -0300

    README.md

--- src/javaBasico/Ex3.java
+++ src/javaBasico/Ex3.java
@@ -2,13 +2,6 @@ package javaBasico;
 
 import java.util.Scanner;
 
-/*
-Elabore um algoritmo em Java, que leia o Salário Bruto,
-o Adicional Noturno, as Horas Extras e os Descontos de um Colaborador,
-em variáveis do tipo float e exiba na tela o Salário Líquido.
-
-SALÁRIO LÍQUIDO = SALÁRIO BRUTO + ADICIONAL NOTURNO + (HORAS EXTRAS * 5) - DESCONTOS
- */
 public class Ex3 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
@@ -29,7 +22,6 @@ public class Ex3 {
 
         salarioLiquido = salarioBruto + (adicionalNoturno + (horasExtras * 5) - descontoCOlaborador);
 
-        //System.out.println("Salario Liquido: " + salarioLiquido);
         System.out.printf("Salario Liquido: %.2f" , salarioLiquido);
 
     }
